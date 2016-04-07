24d6e3a469cc081f86166d94e24883450b100a35
WuXueqian <wuxueqian2010@hotmail.com>
2016-04-07 22:23:53 +0800

    Optimize Segments Touch Slide

--- src/components/tab.jsx
+++ src/components/tab.jsx
@@ -32,40 +32,50 @@ let TabHOC = (TabComponent, TabName='homeTab') => React.createClass({
     translateX: 0,
     preTranslateX: 0,
     segmentIndex: Utils.store(TabName).segmentIndex || 0,
+    segmentWidth: 0,
     containerWidth: 0,
+    segmentCount: 0,
     onTouchStart: function (event){
         event.preventDefault();
 
         this.startT = new Date().getTime();
-        this.containerWidth = event.target.offsetWidth;
+        this.segmentWidth = this.segmentWidth || event.target.offsetWidth;
+        this.containerWidth = this.containerWidth || event.currentTarget.offsetWidth;
+        this.segmentCount = this.segmentCount || (this.containerWidth/this.segmentWidth).toFixed(0)*1;
         this.startX = event.touches[0].pageX;
+        this.preTranslateX = this.segmentIndex * this.segmentWidth * (-1);
+
+        event.currentTarget.style.transitionDuration = '0s';
     },
     onTouchMove: function (event){
         const edgeWith = 0;
         const leftEdge = edgeWith;
-        const rightEdge = -(this.containerWidth + edgeWith);
-        const diffX = event.touches[0].pageX - this.startX;
-        console.log(diffX);
-        this.translateX = this.preTranslateX + diffX;
-        //this.translateX = this.translateX > leftEdge ? edgeWith : this.translateX;
-        //this.translateX = this.translateX < rightEdge ? rightEdge : this.translateX;
+        const rightEdge = -(this.containerWidth - this.segmentWidth + edgeWith);
+        this.diffX = event.touches[0].pageX - this.startX;
+        this.translateX = this.preTranslateX + this.diffX;
+        this.translateX = this.translateX > leftEdge ? edgeWith : this.translateX;
+        this.translateX = this.translateX < rightEdge ? rightEdge : this.translateX;
 
-        event.target.childNodes[1].style.webkitTransform = `translate3d(${this.translateX}px, 0, 0)`;
-        event.target.childNodes[1].style.transform = `translate3d(${this.translateX}px, 0, 0)`;
+        event.currentTarget.style.webkitTransform = `translate3d(${this.translateX}px, 0, 0)`;
+        event.currentTarget.style.transform = `translate3d(${this.translateX}px, 0, 0)`;
     },
     onTouchEnd: function (event){
+        event.currentTarget.style.transitionDuration = '.4s';
+        if (!this.diffX){
+            return;
+        }
         const deltaT = new Date().getTime() - this.startT;
         if(deltaT<300){
-            event.target.childNodes[1].style.webkitTransform = '';
-            event.target.childNodes[1].style.transform = '';
+            this.segmentIndex = Math.max(0, Math.min(this.segmentIndex-(this.diffX/Math.abs(this.diffX)), this.segmentCount-1));
+        }else{
+            this.segmentIndex = Math.abs(this.translateX / this.segmentWidth).toFixed(0);
         }
-        this.segmentIndex = (this.translateX / this.containerWidth).toFixed(0);
-        this.preTranslateX = this.segmentIndex * this.containerWidth;
+        this.preTranslateX = this.segmentIndex * this.segmentWidth * (-1);
         this.setState({
-            currentSegmentIndex: Math.abs(this.segmentIndex)
+            segmentIndex: this.segmentIndex
         });
-        event.target.childNodes[1].style.webkitTransform = `translate3d(${this.preTranslateX}px, 0, 0)`;
-        event.target.childNodes[1].style.transform = `translate3d(${this.preTranslateX}px, 0, 0)`;
+        event.currentTarget.style.webkitTransform = `translate3d(${this.preTranslateX}px, 0, 0)`;
+        event.currentTarget.style.transform = `translate3d(${this.preTranslateX}px, 0, 0)`;
     },
     switchSegmentCallback: function (index){
         this.setState({
@@ -98,21 +108,38 @@ let TabHOC = (TabComponent, TabName='homeTab') => React.createClass({
 let HomeTab = React.createClass({
     displayName: 'HomeTab',
     getSegmentWrapperClassName: function (){
-      switch(this.props.currentSegmentIndex)
-      {
-          case 1:
-              return 'seg-2-of-4';
-              break;
-          case 2:
-              return 'seg-3-of-4';
-              break;
-          case 3:
-              return 'seg-4-of-4';
-              break;
-          default:
-              return 'seg-1-of-4';
-              break;
-      }
+        switch(this.props.currentSegmentIndex)
+        {
+            case 1:
+                return 'seg-2-of-4';
+                break;
+            case 2:
+                return 'seg-3-of-4';
+                break;
+            case 3:
+                return 'seg-4-of-4';
+                break;
+            default:
+                return 'seg-1-of-4';
+                break;
+        }
+    },
+    getSegmentWrapperStyle: function (){
+        switch(this.props.currentSegmentIndex)
+        {
+            case 1:
+                return {transform: 'translate3d(-25%, 0, 0)'};
+                break;
+            case 2:
+                return {transform: 'translate3d(-50%, 0, 0)'};
+                break;
+            case 3:
+                return {transform: 'translate3d(-75%, 0, 0)'};
+                break;
+            default:
+                return {transform: 'translate3d(0, 0, 0)'};
+                break;
+        }
     },
     judgeActiveClassName: function (index){
         if(index==this.props.currentSegmentIndex){
@@ -147,7 +174,7 @@ let HomeTab = React.createClass({
                 {/* Tab Header */}
                 <Header.HomeHeader currentSegmentIndex={this.props.currentSegmentIndex} onSegmentSwitch={this.handleHeaderSegmentSwitch} />
                 {/* Tab内容容器 */}
-                <div id="tab-home-body" className={"tab-view-body transition segments seg-4 " + this.getSegmentWrapperClassName()} onTouchStart={this.props.touchStart} onTouchMove={this.props.touchMove} onTouchEnd={this.props.touchEnd}>
+                <div id="tab-home-body" className={"tab-view-body transition segments seg-4 " + this.getSegmentWrapperClassName()} style={this.getSegmentWrapperStyle()} onTouchStart={this.props.touchStart} onTouchMove={this.props.touchMove} onTouchEnd={this.props.touchEnd}>
                     {/* Segment-直播 */}
                     <section id="segment-live" className={"segment " + this.judgeActiveClassName(0)}>
 
@@ -172,15 +199,17 @@ let HomeTab = React.createClass({
         console.log('Component HomeTab will unmount');
     }
 });
+
 Tab.HomeTab = TabHOC(HomeTab, 'homeTab');
+
 
 /*
  * 关注Tab
  */
-Tab.FocusTab = React.createClass({
+let FocusTab = React.createClass({
     displayName: 'FocusTab',
     getSegmentWrapperClassName: function (){
-        switch(this.state.currentSegmentIndex)
+        switch(this.props.currentSegmentIndex)
         {
             case 1:
                 return 'seg-2-of-3';
@@ -193,34 +222,42 @@ Tab.FocusTab = React.createClass({
                 break;
         }
     },
+    getSegmentWrapperStyle: function (){
+        switch(this.props.currentSegmentIndex)
+        {
+            case 1:
+                return {transform: 'translate3d(calc(-100% / 3), 0, 0)'};
+                break;
+            case 2:
+                return {transform: 'translate3d(calc(-200% / 3), 0, 0)'};
+                break;
+            default:
+                return {transform: 'translate3d(0, 0, 0)'};
+                break;
+        }
+    },
     judgeActiveClassName: function (index){
-        if(index==this.state.currentSegmentIndex){
+        if(index==this.props.currentSegmentIndex){
             return ' active';
         }
         return '';
     },
     handleHeaderSegmentSwitch: function (index){
         console.log('Switch to segment '+ (index+1).toString());
+        if(index==this.props.currentSegmentIndex){
+            return;
+        }
         switch (index)
         {
             case 0:
             case 1:
             case 2:
-                this.setState({
-                    currentSegmentIndex: index
-                });
+                this.props.switchSegmentParentCb(index);
                 break;
             default:
-                this.setState({
-                    currentSegmentIndex: 0
-                });
+                this.props.switchSegmentParentCb(0);
                 break;
         }
-    },
-    getInitialState: function (){
-        return {
-            currentSegmentIndex: 0
-        };
     },
     shouldComponentUpdate: function (nextProps, nextState){
         return this.props != nextProps || this.state != nextState;
@@ -229,9 +266,9 @@ Tab.FocusTab = React.createClass({
         return (
             <section id="tab-focus" className="tab-view">
                 {/* Tab Header */}
-                <Header.FocusHeader onSegmentSwitch={this.handleHeaderSegmentSwitch} />
+                <Header.FocusHeader currentSegmentIndex={this.props.currentSegmentIndex} onSegmentSwitch={this.handleHeaderSegmentSwitch} />
                 {/* Tab内容容器 */}
-                <div id="tab-focus-body" className={"tab-view-body transition segments seg-3 " + this.getSegmentWrapperClassName()}>
+                <div id="tab-focus-body" className={"tab-view-body transition segments seg-3 " + this.getSegmentWrapperClassName()} style={this.getSegmentWrapperStyle()} onTouchStart={this.props.touchStart} onTouchMove={this.props.touchMove} onTouchEnd={this.props.touchEnd}>
                     {/* Segment-追番 */}
                     <section id="segment-track" className={"segment " + this.judgeActiveClassName(0)}>
 
@@ -252,6 +289,8 @@ Tab.FocusTab = React.createClass({
         console.log('Component FocusTab will unmount');
     }
 });
+
+Tab.FocusTab = TabHOC(FocusTab, 'focusTab');
 
 
 /*
@@ -283,6 +322,24 @@ Tab.MeTab = React.createClass({
     shouldComponentUpdate: function (nextProps, nextState){
         return this.props != nextProps || this.state != nextState;
     },
+    getCurrentPosition: function (){
+        if (navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(this.showPosition);
+        }
+    },
+    showPosition: function (position){
+        console.log(position.coords.latitude);
+        this.setState({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+    },
+    getInitialState: function (){
+        return {
+            latitude: '',
+            longitude: ''
+        }
+    },
     render: function (){
         return (
             <section id="tab-me" className="tab-view">
@@ -290,13 +347,17 @@ Tab.MeTab = React.createClass({
                 <Header.MeHeader />
                 {/* Tab内容容器 */}
                 <div id="tab-me-body" className="tab-view-body cells">
-
+                    <div>地理位置为经度: {this.state.longitude ? this.state.longitude : ""}</div>
+                    <div>地理位置为纬度: {this.state.latitude ? this.state.latitude : ""}</div>
                 </div>
             </section>
         );
     },
     componentWillUnmount: function (){
         console.log('Component MeTab will unmount');
+    },
+    componentDidMount: function (){
+        this.getCurrentPosition();
     }
 });
 
