4734c10255768c484b1ae2980cd8d3ce73951343
WuXueqian <wuxueqian2010@hotmail.com>
2016-04-25 22:38:37 +0800

    Do bangumi index tags

--- src/components/bangumiIndexView.jsx
+++ src/components/bangumiIndexView.jsx
@@ -18,17 +18,84 @@
 import React from 'react';
 import Header from './header.jsx';
 import PullRefreshIndicator from './pullRefreshIndicator.jsx';
+import jQuery from 'jquery';
+import Config from '../scripts/config';
 
 
 export default React.createClass({
     displayName: 'BangumiIndexView',
+    getInitialState: function(){
+        return {
+            list: [],
+            page: 1,
+            error: false,
+            isLoad: false
+        };
+    },
+    getTagList: function(){
+        let url = Config.APIType=='json' ? Config.bangumiIndexAPIJSON:Config.bangumiIndexAPIUrl;
+        jQuery.ajax({
+            method: 'get',
+            url: url + '&page=1&pagesize=30',
+            //dataType: 'jsonp',
+            dataType: 'json',
+            context: this,
+            success: function(data){
+                let renderList = [];
+                if(data.hasOwnProperty('message') && data.message=='success' && data.hasOwnProperty('result')){
+                    let result = data.result;
+                    if(!result.length)return;
+                    for(let i=0; i<result.length; i++){
+                        renderList.push(<li key={result[i].tag_id} className="tag" data-index={result[i].index} data-tagId={result[i].tag_id} data-tagName={result[i].tag_name}><img src={result[i].cover} /><span className="tag-name">{result[i].tag_name}</span></li>)
+                    }
+                }
+
+                let loadClear = function(){
+                    if(this.isMounted()){
+                        this.setState({
+                            list: renderList,
+                            isLoad: true
+                        })
+                    }
+                }.bind(this);
+
+                if(!window.load){
+                    setTimeout(function(){
+                        loadClear();
+                    }.bind(this), 500);
+                }else{
+                    loadClear();
+                }
+            },
+            error: function(){
+                this.setState({
+                    error: true
+                });
+            }
+        });
+    },
+    getMoreTagList: function(){
+
+    },
+    componentDidMount: function(){
+        this.getTagList();
+    },
     render: function(){
+        const ErrorWidget = <div className="tag-list error">发生了一个错误</div>;
+        let TagList;
+        if(this.state.error){
+            TagList = ErrorWidget;
+        }else if(this.state.isLoad){
+            TagList = <div className="tag-list">{this.state.list}</div>;
+        }else{
+            TagList = <div className="tag-list" />;
+        }
         return (
             <div id="bangumi-index">
                 <Header.BasicNaviController leftBtnIconClass="left-arrow" leftBtnPath="/backForward" navBarTitle="番剧索引" />
-                <section className="view-body">
+                <section className="view-body" /*onTouchStart={} onTouchMove={} onTouchEnd={}*/>
                     <PullRefreshIndicator pullStatus="unpull" />
-                    bangumi index view
+                    {TagList}
                 </section>
             </div>
         )
---
+++ src/scripts/config.js
@@ -0,0 +1,28 @@
+/**
+ * Copyright 2016, WuXueqian
+ * All right reserved.
+ *
+ * @author WuXueqian
+ * @date 16/4/25 20:23
+ * @license MIT LICENSE
+ */
+
+/*jshint esversion: 6 */
+/*jshint quotmark: false */
+/*jshint white: false */
+/*jshint trailing: false */
+/*jshint newcap: false */
+
+'use strict';
+
+window.app = window.app || {};
+
+app.Config = {
+    APIType: 'json',
+    bangumiIndexAPIUrl: 'http://bangumi.bilibili.com/api/tags?actionKey=appkey&appkey=27eb53fc9058f8c3&build=101130&device=phone&platform=ios&sign=94d9ebfe9a568d0238c78d93fc4c7edb&ts=1461582368',
+    bangumiIndexAPIJSON: 'test/api/bangumi/tags.json?_=_',
+    liveHomeAPIUrl: 'http://live.bilibili.com/mobile/home?actionKey=appkey&appkey=27eb53fc9058f8c3&build=101130&device=phone&platform=ios&scale=2&sign=079a5f6d575506d0fedf59afa13dea04&ts=1461582272',
+    bangumiIndexTagsPerPage: 30
+};
+
+export default app.Config;
